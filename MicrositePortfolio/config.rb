6fa8006ddfac9fd52f25e0bef83183eefe534e89
10ryanna01 <ryann.allahiq@gmail.com>
2016-03-24 20:24:40 +0000

    current site taken from live...this is pre handel bars implimentation

---
+++ MicrositePortfolio/config.rb
@@ -0,0 +1,36 @@
+require 'breakpoint'
+require 'sassy-buttons'
+require 'normalize-scss'
+require 'compass/import-once/activate'
+require 'bootstrap-sass'
+
+
+# Require any additional compass plugins here. 
+ 
+# Set this to the root of your project when deployed:
+
+ 
+http_path = "/"
+css_dir = "web/resources/stylesheets"
+sass_dir = "app/resources/scss"
+images_dir = "web/resources/images"
+javascripts_dir = "app/resources/js"
+fonts_dir = "web/resources/fonts"
+ 
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+  relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+  
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass app/resources/scss scss && rm -rf sass && mv scss sass
+asset_cache_buster :none
---
+++ MicrositePortfolio/web/resources/js/init.js
@@ -0,0 +1,12 @@
+// contents of init.js:
+
+head.load("./web/resources/js/libs/jquery-1.11.3.min.js",
+	'./web/resources/js/libs/mainfunctions.js',
+	'./web/resources/js/libs/modernizr.custom.js',
+	'./web/resources/js/libs/bootstrap.js',
+	'./web/resources/js/libs/jquery.mixitup.js',
+	'./web/resources/js/libs/jquery.fancybox.js');
+
+
+
+head.load(["http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"]);
